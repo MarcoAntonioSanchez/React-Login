c61368449deca1c83defa27a4ae7ea326b815bcd
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-08-29 00:27:47 -0500

    05. Importing reac-router-dom and components to app.js

--- state-class-tutorial/src/components/App/App.js
+++ state-class-tutorial/src/components/App/App.js
@@ -1,4 +1,8 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
+import Preferences from "../Preferences/Preferences";
 
 function App() {
   return <></>;
